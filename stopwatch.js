ccc7e3610e96ae5dfe6e125f2c724d3fcd48afa8
ksweety <kahlilw03@gmail.com>
2023-04-28 14:55:46 -0400

    ff

---
+++ stopwatch.js
@@ -0,0 +1,10 @@
+function start () {
+    alert("You clicked the start button")
+} 
+function stop () {
+    alert("You have clicked the stop button")
+}
+
+function reset () {
+    alert("You have clicked the reset button")
+}
